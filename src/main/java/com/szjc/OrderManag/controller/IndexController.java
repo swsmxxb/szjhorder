f7eb8dacf6da4bba3eee96b9cc87abc4c1535080
徐晓波 <xuxiaobocick@163.com>
2018-09-14 08:30:12 +0800

    first commit

---
+++ src/main/java/com/szjc/OrderManag/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.szjc.OrderManag.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String mainpage(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/tables")
+    public String tables(Model model) {
+        return "table_bootstrap";
+    }
+}
