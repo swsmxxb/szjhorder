957b8ae6af1aa998723c14ab6c1996b2a58315b9
徐晓波 <xuxiaobocick@163.com>
2018-09-19 17:19:01 +0800

    接口请求test

--- src/main/java/com/szjc/OrderManag/OrderManagApplication.java
+++ src/main/java/com/szjc/OrderManag/OrderManagApplication.java
@@ -1,12 +1,30 @@
 package com.szjc.OrderManag;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 @SpringBootApplication
 public class OrderManagApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnirestException {
 		SpringApplication.run(OrderManagApplication.class, args);
+
+		try {
+			String requestBody = "{\"id\":\"123\"}";
+			HttpResponse<String> responseApi = Unirest.post("http://192.168.1.120:8080").header("cache-control", "no-cache")
+					.body(requestBody).asString();
+			String jsData= JSONObject.fromObject(responseApi.getBody()).getString("subscribe_data");
+
+			System.out.println(jsData);
+		}catch (Exception e  ) {
+
+
+			e.printStackTrace();
+		}
 	}
 }
